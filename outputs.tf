9676bee99cf912ac7fd4a91fe29181bf180fad9c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "automation_modules" {
-  description = "All automation_module resources"
-  value       = azurerm_automation_module.automation_modules
-}
 output "automation_modules_automation_account_name" {
-  description = "List of automation_account_name values across all automation_modules"
-  value       = [for k, v in azurerm_automation_module.automation_modules : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_modules, keyed the same as var.automation_modules"
+  value       = { for k, v in azurerm_automation_module.automation_modules : k => v.automation_account_name }
 }
 output "automation_modules_module_link" {
-  description = "List of module_link values across all automation_modules"
-  value       = [for k, v in azurerm_automation_module.automation_modules : v.module_link]
+  description = "Map of module_link values across all automation_modules, keyed the same as var.automation_modules"
+  value       = { for k, v in azurerm_automation_module.automation_modules : k => v.module_link }
 }
 output "automation_modules_name" {
-  description = "List of name values across all automation_modules"
-  value       = [for k, v in azurerm_automation_module.automation_modules : v.name]
+  description = "Map of name values across all automation_modules, keyed the same as var.automation_modules"
+  value       = { for k, v in azurerm_automation_module.automation_modules : k => v.name }
 }
 output "automation_modules_resource_group_name" {
-  description = "List of resource_group_name values across all automation_modules"
-  value       = [for k, v in azurerm_automation_module.automation_modules : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_modules, keyed the same as var.automation_modules"
+  value       = { for k, v in azurerm_automation_module.automation_modules : k => v.resource_group_name }
 }
 
